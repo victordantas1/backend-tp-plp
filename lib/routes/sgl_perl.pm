b0d6ac433496d96881edac8e99dccda1a2289f8e
victordantas1 <victorr.dantas@gmail.com>
2024-12-06 20:25:41 -0400

    Adiciona upload de varios arquivos

--- lib/routes/sgl_perl.pm
+++ lib/routes/sgl_perl.pm
@@ -4,34 +4,48 @@ use Text::CSV;
 use JSON;
 
 post '/upload' => sub {
-    # Recebe o arquivo da requisição
-    my $upload = upload('file');
-    unless ($upload) {
+    # Recebe os arquivos enviados no campo 'files'
+    my @uploads = upload('files');
+    
+    unless (@uploads) {
         return status 400, to_json({ error => "Nenhum arquivo foi enviado." });
     }
 
-    # Lê o conteúdo do arquivo
-    my $content = $upload->content;
-    my @lines = split /\n/, $content;
+    my @results; # Armazena os resultados de cada arquivo
 
-    # Regex para capturar IPs
-    my @ips;
-    foreach my $line (@lines) {
-        while ($line =~ /(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)/g) {
-            push @ips, $1;
+    # Iterar sobre os uploads
+    foreach my $upload (@uploads) {
+        my $filename = $upload->filename; # Nome do arquivo original
+        
+        my $csv_file = 'csv/ips_extracted_' . $filename . '.csv';
+        my $csv = Text::CSV->new({ binary => 1, eol => $/});
+
+        # Abrir arquivo CSV para salvar os IPs
+        open my $fh, '>', $csv_file or die "Não foi possível abrir $csv_file: $!";
+        $csv->print($fh, ['Filename', 'IP']); # Cabeçalho do CSV
+
+        my $content = $upload->content;
+        my @lines = split /\n/, $content;
+
+        my @ips;
+        foreach my $line (@lines) {
+            while ($line =~ /(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)/g) {
+                push @ips, $1;
+            }
         }
+
+        # Salvar IPs e o nome do arquivo no CSV
+        foreach my $ip (@ips) {
+            $csv->print($fh, [$filename, $ip]);
+        }
+
+        # Armazenar resultados para o JSON final
+        push @results, { filename => $filename, ips => \@ips };
+        close $fh;
     }
 
-    # Salvar IPs no arquivo CSV
-    my $csv_file = 'ips_extracted.csv';
-    my $csv = Text::CSV->new({ binary => 1, eol => $/ });
-    open my $fh, '>', $csv_file or die "Não foi possível abrir $csv_file: $!";
-    $csv->print($fh, ['IP']);
-    $csv->print($fh, [$_]) for @ips;
-    close $fh;
-
-    # Retorna os IPs extraídos em formato JSON
-    return to_json({ ips_extracted => \@ips });
+    # Retorna os resultados para o cliente
+    return to_json({ files => \@results });
 };
 
 # Inicia o servidor
