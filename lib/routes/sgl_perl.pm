138b33b9e181a00d4b389e4538a8ccda208b2f6e
victordantas1 <victorr.dantas@gmail.com>
2024-12-08 19:19:45 -0400

    Adiciona funcao ao routes

--- lib/routes/sgl_perl.pm
+++ lib/routes/sgl_perl.pm
@@ -11,6 +11,37 @@ hook after => sub {
     $response->header('Access-Control-Allow-Headers' => 'Content-Type, Authorization');
 };
 
+sub process_line {
+    my ($line) = @_;
+
+    # Valores padrão
+    my ($date, $time, $ip, $gpu, $cpu, $ram, $disk) = ('N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A');
+
+    # Regex flexível para capturar quaisquer campos disponíveis
+    if ($line =~ /(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})/) {
+        $date = $1;
+        $time = $2;
+    }
+    if ($line =~ /Endere.*IP:\s([\d\.]+)/) {
+        $ip = $1;
+    }
+    if ($line =~ /GPU:\s([\d\.]+)%/) {
+        $gpu = $1;
+    }
+    if ($line =~ /Media de CPU:\s([\d\.]+)%/) {
+        $cpu = $1;
+    }
+    if ($line =~ /RAM:\s([\d\.]+)%/) {
+        $ram = $1;
+    }
+    if ($line =~ /Disco:\s([\d\.]+)%/) {
+        $disk = $1;
+    }
+
+    # Retornar os valores extraídos
+    return ($date, $time, $ip, $gpu, $cpu, $ram, $disk);
+}
+
 post '/upload' => sub {
     my @uploads = upload('files');
     
@@ -35,10 +66,25 @@ post '/upload' => sub {
         my @lines = split /\n/, $content;
 
         my @ips;
+        my @date;
+        my @time;
+        my @ip;
+        my @gpu;
+        my @cpu;
+        my @ram;
+        my @disk;
+
+        my ($date, $time, $ip, $gpu, $cpu, $ram, $disk) = ('N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A');
         foreach my $line (@lines) {
             while ($line =~ /(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)/g) {
                 push @ips, $1;
             }
+            ($date, $time, $ip, $gpu, $cpu, $ram, $disk) = process_line($line);
+            push @ip, $ip;
+            push @gpu, $gpu;
+            push @cpu, $cpu;
+            push @ram, $ram;
+            push @disk, $disk;
         }
 
         # Salvar IPs e o nome do arquivo no CSV
@@ -47,7 +93,13 @@ post '/upload' => sub {
         }
 
         # Armazenar resultados para o JSON final
-        push @results, { filename => $filename, ips => \@ips };
+        push @results, { filename => $filename, 
+            ip => \@ips,
+            ram => \@ram,
+            gpu => \@gpu,
+            cpu => \@cpu,
+            disk => \@disk
+        };
         close $fh;
     }
 
